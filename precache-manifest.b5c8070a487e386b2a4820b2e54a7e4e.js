51947c58ce64277ee2abeac538935b2298ffb4dd
Danielle Burrage <burragedanielle@gmail.com>
2021-01-03 23:03:13 -0700

    Updates

---
+++ precache-manifest.b5c8070a487e386b2a4820b2e54a7e4e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f9afc21583092547a520d43a566339c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4104fdabdb636b492ab7",
+    "url": "/static/css/main.6315b1a8.chunk.css"
+  },
+  {
+    "revision": "3ac6b43a12db8326655d",
+    "url": "/static/js/2.1b29b5f8.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/static/js/2.1b29b5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4104fdabdb636b492ab7",
+    "url": "/static/js/main.8721df8f.chunk.js"
+  },
+  {
+    "revision": "42ede37a1bce51baed9e",
+    "url": "/static/js/runtime-main.aeb3bcad.js"
+  },
+  {
+    "revision": "02717457a93b24d798c78706f288ecb6",
+    "url": "/static/media/Jaceline.02717457.ttf"
+  },
+  {
+    "revision": "956885da8003ff6a32253d15ee36f8e9",
+    "url": "/static/media/PalmerLakePrint-Regular.956885da.woff"
+  },
+  {
+    "revision": "248af44e378f58019b34d4d45b926b46",
+    "url": "/static/media/allocat_screenshot.248af44e.png"
+  },
+  {
+    "revision": "f4872588f5934e2db1f9aff418660194",
+    "url": "/static/media/background-contact-page.f4872588.png"
+  },
+  {
+    "revision": "655265cfc5fc8b03c5bd8faae3b06df0",
+    "url": "/static/media/mainlanding.655265cf.png"
+  },
+  {
+    "revision": "c0bfbf6d81b384be64a3ffa8bbe0f16f",
+    "url": "/static/media/mainlandingmobile.c0bfbf6d.png"
+  },
+  {
+    "revision": "b2f1cc70fe05716aed864a7ee04a9688",
+    "url": "/static/media/portfolio-about-me-pictures.b2f1cc70.png"
+  },
+  {
+    "revision": "4c529b1761e610be7609e15151494dd7",
+    "url": "/static/media/portfolio-textemphasis.4c529b17.png"
+  },
+  {
+    "revision": "c6778acbaf3770d4db02f094b0b1f3c9",
+    "url": "/static/media/schedulez_screenshot.c6778acb.png"
+  }
+]);
