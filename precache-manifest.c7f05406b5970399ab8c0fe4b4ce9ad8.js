23af3687e72100daee6ffffcaf4a645309de7b65
Danielle Burrage <burragedanielle@gmail.com>
2021-01-25 17:03:26 -0600

    Updates

--- precache-manifest.c7f05406b5970399ab8c0fe4b4ce9ad8.js
+++ precache-manifest.c7f05406b5970399ab8c0fe4b4ce9ad8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "de7d888595aa2cd7c53ac50e08799288",
+    "revision": "6d2025dbc26ea088810057487e9e4eeb",
     "url": "/index.html"
   },
   {
-    "revision": "a2aedb0f55f38ef28ab3",
+    "revision": "e0f7e72c50fd91e8d602",
     "url": "/static/css/2.4707e12a.chunk.css"
   },
   {
-    "revision": "aea759145cb96e5c5ab2",
-    "url": "/static/css/main.2f141e42.chunk.css"
+    "revision": "cefe92eb40d5059cf87b",
+    "url": "/static/css/main.a664862d.chunk.css"
   },
   {
-    "revision": "a2aedb0f55f38ef28ab3",
-    "url": "/static/js/2.c92015f9.chunk.js"
+    "revision": "e0f7e72c50fd91e8d602",
+    "url": "/static/js/2.ec542522.chunk.js"
   },
   {
     "revision": "44ba416b9a7217ca99c025d9f4dd1124",
-    "url": "/static/js/2.c92015f9.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.ec542522.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aea759145cb96e5c5ab2",
-    "url": "/static/js/main.3c729b81.chunk.js"
+    "revision": "cefe92eb40d5059cf87b",
+    "url": "/static/js/main.e306107c.chunk.js"
   },
   {
     "revision": "42ede37a1bce51baed9e",
@@ -44,16 +44,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/media/background-contact-page.f4872588.png"
   },
   {
-    "revision": "89ac7644b5fab169a1db1073cb344491",
-    "url": "/static/media/burrage_danielle-resume2021.89ac7644.pdf"
+    "revision": "df148ea2b08e0e7f03e6c8c033cd744d",
+    "url": "/static/media/background-hero.df148ea2.jpg"
+  },
+  {
+    "revision": "08b93b20f824ffa5f2888ab64d5a8c6c",
+    "url": "/static/media/burragedanielleresume.08b93b20.pdf"
   },
   {
     "revision": "fdcae3f9c5f4e3de4bcef12885271753",
     "url": "/static/media/petshop_screenshots.fdcae3f9.png"
-  },
-  {
-    "revision": "b2f1cc70fe05716aed864a7ee04a9688",
-    "url": "/static/media/portfolio-about-me-pictures.b2f1cc70.png"
   },
   {
     "revision": "4c529b1761e610be7609e15151494dd7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.80084c313b3f8ca50d96a7c403208de7.js"
+  "/precache-manifest.c7f05406b5970399ab8c0fe4b4ce9ad8.js"
 );
 
 self.addEventListener('message', (event) => {
